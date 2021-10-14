6901f293112b59bdefac0c3fa27e32db4cfce257
Mario Parrilla Maroto <marioparrilla2@gmail.com>
2021-10-14 13:35:52 +0200

    Menu Settings Añadido

--- app/src/main/java/com/marioparrillamaroto/myeventsapp/MainActivity.java
+++ app/src/main/java/com/marioparrillamaroto/myeventsapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.marioparrillamaroto.myeventsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -22,6 +24,9 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recView;
+
+    private AppBarConfiguration appBarConfiguration2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +46,11 @@ public class MainActivity extends AppCompatActivity {
         recView.setLayoutManager(lym);
         recView.setAdapter(adapterData);
 
+
+        NavController navController2 = Navigation.findNavController(this, R.id.navContainer);
+        appBarConfiguration2 = new AppBarConfiguration.Builder(navController2.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController2, appBarConfiguration2);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -50,5 +60,36 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.navContainer);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent nuevaPantalla = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(nuevaPantalla);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.navContainer);
+        return NavigationUI.navigateUp(navController, appBarConfiguration2)
+                || super.onSupportNavigateUp();
     }
 }
